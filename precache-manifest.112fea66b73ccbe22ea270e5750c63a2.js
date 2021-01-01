af239f8e8be672ecc1132efb47d468749dfe6cac
Yuan Fang <yxf313@miami.edu>
2021-01-01 17:50:13 -0500

    Updates

---
+++ precache-manifest.112fea66b73ccbe22ea270e5750c63a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1de4fddefb0d55f57c77733692a6b6",
+    "url": "/world-on-cover/index.html"
+  },
+  {
+    "revision": "9233970e57161c2c0b60",
+    "url": "/world-on-cover/static/css/2.4c356872.chunk.css"
+  },
+  {
+    "revision": "0f1a65f8855ae18e668a",
+    "url": "/world-on-cover/static/css/main.551d871a.chunk.css"
+  },
+  {
+    "revision": "9233970e57161c2c0b60",
+    "url": "/world-on-cover/static/js/2.3c11cbaf.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/world-on-cover/static/js/2.3c11cbaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1a65f8855ae18e668a",
+    "url": "/world-on-cover/static/js/main.dfcc4aa7.chunk.js"
+  },
+  {
+    "revision": "c2f0176874fd2390750e",
+    "url": "/world-on-cover/static/js/runtime-main.dfe0b4e6.js"
+  },
+  {
+    "revision": "15d2860858b8494cd48691f7d67e92a5",
+    "url": "/world-on-cover/static/media/header_img.15d28608.png"
+  },
+  {
+    "revision": "4b42269c0974b97fa48c16c0127abbf8",
+    "url": "/world-on-cover/static/media/mz.4b42269c.png"
+  },
+  {
+    "revision": "9830d12c934cc4ee7750f2752bac62f6",
+    "url": "/world-on-cover/static/media/timeline.9830d12c.svg"
+  }
+]);
